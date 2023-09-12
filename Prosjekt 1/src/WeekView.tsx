d4d2683495db1416d1096d310c9fe97e8581ab19
sofieothilie <sofieod@stud.ntnu.no>
2023-09-12 17:30:39 +0200

    Updates weekview to use actual data from api

--- Prosjekt 1/src/WeekView.tsx
+++ Prosjekt 1/src/WeekView.tsx
@@ -1,19 +1,50 @@
+import { ReactElement } from "react";
 import "./WeekView.css";
 
-export default function WeekView() {
-  const days = [];
-
-  for (let i = 0; i < 7; i++) {
-    days.push(
-      <div className="days">
-        <p>
-          23.05.23 <br /> Mandag
-        </p>
-        <img src={"icon.png"} className="icon" alt="icon" />
-        <p>24 °C</p>
-      </div>
-    );
+export default function weekView(data: any) {
+  interface ForecastItem {
+    date: string;
+    day: string;
+    temp: string;
+    icon: string;
   }
 
-  return <div className="week">{days}</div>;
+  let days: ReactElement<any, any>[] = [];
+
+  let DAYS: { [id: number]: string } = {
+    1: "Mandag",
+    2: "Tirdag",
+    3: "Onsdag",
+    4: "Tordag",
+    5: "Fredag",
+    6: "Lørdag",
+    7: "Søndag",
+  };
+
+  const forecastList: ForecastItem[] = data?.forecast?.forecastday.map(
+    (day: any) => ({
+      date: day.date,
+      day: DAYS[new Date(day.date).getDay()],
+      temp: `${day.day.maxtemp_c} °C`,
+      icon: day.day.condition.icon,
+    })
+  );
+
+  console.log(forecastList);
+
+  forecastList?.forEach((element: ForecastItem) => {
+    days.push(
+      <div className="days" key="{item}">
+        <p>
+          {element.date} <br /> {element.day}
+        </p>
+        <img src={element.icon} className="icon" alt="icon" />
+        <p>{element.temp}</p>
+      </div>
+    );
+  });
+
+  return <div>
+    {forecastList ? <div className="week">{days}</div> : <></>}
+  </div>;
 }
