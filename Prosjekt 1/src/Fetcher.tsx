88ca26c1e3ca6f2817eb83e519c492280149f54e
jespeste <jespeste@stud.ntnu.no>
2023-09-17 23:32:50 +0200

    Added dependent querying for MET

--- Prosjekt 1/src/Fetcher.tsx
+++ Prosjekt 1/src/Fetcher.tsx
@@ -2,48 +2,58 @@ import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import weekView from "./pages/WeekView";
 import "./App.css";
+import { Root, Geo } from "./types";
 
 function Fetcher() {
   const [city, setCity] = useState<string>();
-  const [printCity, setPrintCity] = useState<string | unknown>("No City");
-
-  const geoCode: string = `https://eu1.locationiq.com/v1/search?key=pk.3e21916e151f4d42374fdc631eded07a&q=${printCity}&format=json`;
-  const metApiCall: string = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${lat}&lon=${lon}`;
-
-
-  const baseAPIcall: string =
-    "https://api.weatherapi.com/v1/forecast.json?key=7a8579bd6b1743c299b82117230709&q=";
-  const endQuery: string = "&days=3&aqi=no&alerts=no";
-
+  const [printCity, setPrintCity] = useState<string | unknown>();
+  
   const fetchGeocode = (): Promise<Geo | void> => fetch(geoCode, {
     credentials: "same-origin",
   })
-    .then((res) => res.json()).then((data) => data as Geo)
+    .then((res) => res.json()).then((res)=>res[0]).then((data) => data as Geo)
     .catch((error) => console.log(error));
 
-  const fetchMetData = (): Promise<WeatherData | void> => 
-      fetch(metApiCall).then((res)=> res.json()).then((data)=> data as WeatherData)
-
+  const fetchMetData = (): Promise<Root | void> => 
+      fetch(metApiCall).then((res)=> res.json()).then((data)=> data as Root);
+  
   const { status: statusLocation, data: geoCodeData } = useQuery({
     queryKey: ["geoCode", printCity],
     queryFn: fetchGeocode,
     refetchOnMount: false,
     refetchOnWindowFocus: false,
-    cacheTime: 3000,
   });
+  
 
+  const geoCode: string = `https://eu1.locationiq.com/v1/search?key=pk.3e21916e151f4d42374fdc631eded07a&q=${printCity}&format=json`;
+  const lon = geoCodeData?.lon
+  const lat = geoCodeData?.lat
+  
+  const metApiCall: string = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${lat}&lon=${lon}`;
+  
   const { status: statusMetCall, data: metData } = useQuery({
-    queryKey: ["geoCode", printCity],
+    queryKey: ["metData", printCity],
+    queryFn: fetchMetData,
     refetchOnMount: false,
     refetchOnWindowFocus: false,
+    enabled: !!lat,
   })
+  
+  
+  
+  const baseAPIcall: string =
+    "https://api.weatherapi.com/v1/forecast.json?key=7a8579bd6b1743c299b82117230709&q=";
+  const endQuery: string = "&days=3&aqi=no&alerts=no";
+
+
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
     setPrintCity(city);
   };
 
-  if (status === "error") return <span>Currently struggling</span>;
+  if (statusLocation === "error") return <span>Could not find location...</span>;
+  if (statusMetCall === "error") return <span>Could not fetch weather data...</span>
 
   return (
     <div className="searchBox">
@@ -63,9 +73,7 @@ function Fetcher() {
       </form>
     </div>
     <div className="favorites"><h1>Favoritter</h1></div>
-      <h1>{data?.location?.name}</h1>
-      {isLoading ? <span>Loading...</span> : <></>}
-      {data ? weekView(data) : <></>}
+      <ul>{statusMetCall === "loading" ? <p>Loading...</p> : metData?.properties.timeseries.map((day)=><p>{day.time}</p>)}</ul>
     </div>
   );
 }
