48b7a95281c7eb9f327576dae0bc279c652c41ee
jespeste <jespeste@stud.ntnu.no>
2023-09-17 22:41:38 +0200

    Added some new functions and types

--- Prosjekt 1/src/Fetcher.tsx
+++ Prosjekt 1/src/Fetcher.tsx
@@ -7,23 +7,36 @@ function Fetcher() {
   const [city, setCity] = useState<string>();
   const [printCity, setPrintCity] = useState<string | unknown>("No City");
 
+  const geoCode: string = `https://eu1.locationiq.com/v1/search?key=pk.3e21916e151f4d42374fdc631eded07a&q=${printCity}&format=json`;
+  const metApiCall: string = `https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${lat}&lon=${lon}`;
+
+
   const baseAPIcall: string =
     "https://api.weatherapi.com/v1/forecast.json?key=7a8579bd6b1743c299b82117230709&q=";
   const endQuery: string = "&days=3&aqi=no&alerts=no";
 
-  const { status, data, isLoading } = useQuery({
-    queryKey: ["dayandtemp", printCity],
-    queryFn: async () => {
-      return fetch(baseAPIcall + city + endQuery, {
-        credentials: "same-origin",
-      })
-        .then((res) => res.json())
-        .catch((error) => console.log(error));
-    },
+  const fetchGeocode = (): Promise<Geo | void> => fetch(geoCode, {
+    credentials: "same-origin",
+  })
+    .then((res) => res.json()).then((data) => data as Geo)
+    .catch((error) => console.log(error));
+
+  const fetchMetData = (): Promise<WeatherData | void> => 
+      fetch(metApiCall).then((res)=> res.json()).then((data)=> data as WeatherData)
+
+  const { status: statusLocation, data: geoCodeData } = useQuery({
+    queryKey: ["geoCode", printCity],
+    queryFn: fetchGeocode,
     refetchOnMount: false,
     refetchOnWindowFocus: false,
     cacheTime: 3000,
   });
+
+  const { status: statusMetCall, data: metData } = useQuery({
+    queryKey: ["geoCode", printCity],
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+  })
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
--- Prosjekt 1/src/types.ts
+++ Prosjekt 1/src/types.ts
@@ -2,6 +2,15 @@ interface Day {
     day: Record<string, JSONArray>
 }
 
+interface Geo {
+    lat: string,
+    lon: string
+}
+
+interface WeatherData {
+    date: string
+}
+
 interface Hour {
     temp_c: string,
     time: string,
@@ -13,6 +22,7 @@ interface Condition {
 }
 
 type JSONvalue = string | number | string[] | Array<JSONvalue> | Hour;
+
 
 
 interface JSONArray extends Array<Hour> { }
