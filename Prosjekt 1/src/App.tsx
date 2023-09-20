a019e0072e469ba03b4f4dfbb30c807a3ecadb5d
Lars Tønder <larstond@stud.ntnu.no>
2023-09-20 23:19:01 +0200

    fix routing and add toolbar

--- Prosjekt 1/src/App.tsx
+++ Prosjekt 1/src/App.tsx
@@ -1,25 +1,55 @@
 import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Header from "./Header";
 import Root from "./routes/root";
 import Location from "./routes/location";
-import Favourites from "./Favourite";
+import { useState } from "react";
 
 const queryClient = new QueryClient();
 
 function App() {
+  const [city, setCity] = useState<string>();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (city) {
+        navigate(`/project1/${city}`);
+    } else {
+        console.log("Cannot pass undefined")
+    }
+  };
+
   return (
     <div className="main-root">
       <Header />
       <div className="content">
         <QueryClientProvider client={queryClient}>
+          <div className="toolbar">
+            <button onClick={() => navigate("/project1")}>Hjem</button>
+            <div className="search">
+              <form onSubmit={(e) => handleSubmit(e)}>
+                <label>
+                <input
+                    type="text"
+                    className="searchbar"
+                    placeholder="Skriv inn en by"
+                    onChange={(e) => {
+                    setCity(e.target.value);
+                    }}
+                ></input>
+                </label>
+                <input type="submit" placeholder="Hei" className="submitButton"></input>
+            </form>
+          </div>
+        </div>
           <Routes>
             <Route path="/project1" element={<Root />} />
             <Route path="/project1/:id" element={<Location />} />
           </Routes>
-          <Favourites/>
           <ReactQueryDevtools></ReactQueryDevtools>
         </QueryClientProvider>
       </div>
