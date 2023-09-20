57c92a749d206d1172d7ee777cf6c56a81cb0df8
sofieothilie <sofieod@stud.ntnu.no>
2023-09-20 21:43:51 +0200

    Adds popup styling to component

--- Prosjekt 1/src/pages/DetailedWeekView.tsx
+++ Prosjekt 1/src/pages/DetailedWeekView.tsx
@@ -1,13 +1,18 @@
+import React, { useState } from "react";
 import FetchIcon from "../FetchIcon";
 import "./DetailedWeekView.css";
 import { DayView, Properties } from "../types";
 import HourlyView from "../components/HourlyView";
+import "./Popup.css";
 
-export default function DetailedWeekView(props: {dayView: DayView, rawData: Properties}) {
-  
-  const dayView = props.dayView
-  const metData = props.rawData
-  
+
+export default function DetailedWeekView(props: {
+  dayView: DayView;
+  rawData: Properties;
+}) {
+  const dayView = props.dayView;
+  const metData = props.rawData;
+
   const DAYS: { [id: number]: string } = {
     1: "Mandag",
     2: "Tirdag",
@@ -18,6 +23,22 @@ export default function DetailedWeekView(props: {dayView: DayView, rawData: Prop
     7: "Søndag",
   };
 
+  // Step 1: Define state variables to manage the popup
+  const [isPopupVisible, setPopupVisible] = useState(false);
+  const [selectedDay, setSelectedDay] = useState("");
+
+  // Step 2: Function to show the popup
+  const showPopup = (day: string) => {
+    setSelectedDay(day);
+    setPopupVisible(true);
+  };
+
+  // Function to hide the popup
+  const hidePopup = () => {
+    setSelectedDay("");
+    setPopupVisible(false);
+  };
+
   return (
     <div className="detcomp">
       <div className="detrow">
@@ -26,15 +47,38 @@ export default function DetailedWeekView(props: {dayView: DayView, rawData: Prop
         <p className="detcell">Min/Max. temp.</p>
         <p className="detcell">Nedbør</p>
         <p className="detcell">Vind</p>
+        <p className="detcell"></p>
       </div>
-      {dayView?.map((day) => 
+      {dayView?.map((day) => (
         <div className="detrow">
-          <p className="detcell">{day.date} {DAYS[new Date(day.date).getDay()]}</p>
-          <div className="detcell">{day.symbol ? FetchIcon(day.symbol) : FetchIcon("clearsky_day")}</div>
-          <p className="detcell">{day.maxTemp} {"\u00B0"}C</p>
-          <p className="detcell">{day.rain}</p>
+          <p className="detcell">
+            {day.date.split("-")[2]}
+            {"."}
+            {day.date.split("-")[1]} {DAYS[new Date(day.date).getDay()]}
+          </p>
+          <div className="detcell">
+            {day.symbol ? FetchIcon(day.symbol) : FetchIcon("clearsky_day")}
+          </div>
+          <p className="detcell">
+            {day.maxTemp} {"\u00B0"}C
+          </p>
+          <p className="detcell">{day.rain} mm</p>
           <p className="detcell">{day.wind} m/s</p>
-          <p className="detcell"><HourlyView metData={metData} showBoolean={false} day={day.date}></HourlyView></p>
+          <p className="detcell">
+            <button onClick={() => showPopup(day.date)}>Se været</button>
+          </p>
+        </div>
+      ))}
+
+      {/* Render HourlyView as a popup when isPopupVisible is true */}
+      {isPopupVisible && (
+        <div className="popup-overlay">
+          <HourlyView
+            metData={metData}
+            showBoolean={true}
+            day={selectedDay}
+          />
+          <button className="close-popup" onClick={hidePopup}>Close</button>
         </div>
       )}
     </div>
