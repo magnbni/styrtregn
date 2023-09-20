164e8c27611b1f93b495bd97311cea3c53d2a41a
sofieothilie <sofieod@stud.ntnu.no>
2023-09-20 16:46:57 +0200

    Further implements detailed week
    Fixes #18

--- Prosjekt 1/src/pages/DetailedWeekView.tsx
+++ Prosjekt 1/src/pages/DetailedWeekView.tsx
@@ -1,7 +1,7 @@
 import "./DetailedWeekView.css";
+import clearsky from "../weathericons-svg/clearsky_day.svg";
 
 export default function DetWeekView() {
-  
 
   return (
     <div className="detcomp">
@@ -11,13 +11,19 @@ export default function DetWeekView() {
         <p className="detcell">Morgen</p>
         <p className="detcell">Ettermiddag</p>
         <p className="detcell">Kveld</p>
-        <p className="detcell">Maks/min. temp.</p>
+        <p className="detcell">Min/Max. temp.</p>
         <p className="detcell">Nedbør</p>
         <p className="detcell">Vind</p>
       </div>
       <div className="detrow">
-        <p className="detcell">Detaljert</p>
-        <p className="detcell">Detaljert</p>
+        <p className="detcell">11.02.23 Mandag</p>
+        <img src={clearsky} className="detcell" alt="icon" />
+        <p className="detcell">Icon 2</p>
+        <p className="detcell">Icon 3</p>
+        <p className="detcell">Icon 4</p>
+        <p className="detcell">11/14 {"\u00B0"}C</p>
+        <p className="detcell">1,2 mm</p>
+        <p className="detcell">4 m/s</p>
       </div>
     </div>
   );
