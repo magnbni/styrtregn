fc76e0e5700faad5ce295da557407a5f42ffd6bc
Magnus Brekke Nilsen <magnbni@stud.ntnu.no>
2023-09-21 15:07:24 +0200

    Fixed issues related to ESLint

--- Prosjekt 1/src/routes/root.tsx
+++ Prosjekt 1/src/routes/root.tsx
@@ -1,8 +1,4 @@
-import { useEffect, useState } from "react";
 import "../index.css";
-import useFetchCity from "../useFetchCity";
-import HourlyView from "../components/HourlyView";
-import DetailedWeekView from "../pages/DetailedWeekView";
 import Favourites from "../Favourite";
 
 export default function Root() {
