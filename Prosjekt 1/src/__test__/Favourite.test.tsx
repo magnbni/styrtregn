de8fb87a68393158e77bc32ed251055e1aa1851c
Lars Tønder <larstonder@users.noreply.github.com>
2023-09-21 22:55:23 +0200

    add HourlyView test and fix tests

--- Prosjekt 1/src/__test__/Favourite.test.tsx
+++ Prosjekt 1/src/__test__/Favourite.test.tsx
@@ -3,10 +3,15 @@ import { render } from "@testing-library/react";
 import FavoriteView from "../components/FavouriteView";
 import { mswServer } from "../test/mockHTTPserver";
 import { fetchCity } from "../test/handlers";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 it("Favourite should render", async () => {
+  const queryClient = new QueryClient();
   mswServer.use(fetchCity);
-  const wrapper = render(<FavoriteView city="Oslo"></FavoriteView>);
+  const wrapper = render(
+    <QueryClientProvider client={queryClient}>
+      <FavoriteView city="Oslo"/>
+    </QueryClientProvider>);
 
   expect(wrapper).toBeTruthy;
 });
